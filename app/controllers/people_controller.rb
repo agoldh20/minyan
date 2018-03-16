dc67a118c95376d97a4a1fad50423d5901984eec
agoldh20 <a.goldh20@gmail.com>
2018-03-15 22:37:20 -0500

    inital commit(2)

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,15 @@ class PeopleController < ApplicationController
     @people = Person.all
   end
 
+  def create
+    @person = Person.create(
+                            name: params[:name],
+                            attending: false)
+    redirect_to '/people'
+  end
+
   def attending
     @people = Person.all
+    @people = @people.order(:name)
   end
 end
