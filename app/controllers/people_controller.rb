450e0ec453d8258d5a9b3f1b48dbae80a657aae1
agoldh20 <a.goldh20@gmail.com>
2018-03-15 23:11:07 -0500

    sort names

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,11 +3,19 @@ class PeopleController < ApplicationController
     @people = Person.all
   end
 
+  def new
+    @person = Person.new
+  end
+
   def create
     @person = Person.create(
                             name: params[:name],
                             attending: false)
     redirect_to '/people'
+  end
+
+  def edit
+    @person = Person.find(params[:id])
   end
 
   def attending
